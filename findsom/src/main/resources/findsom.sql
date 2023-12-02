ae3b2b68584402bd8842114304579befa605f4fa
dasol729 <97594634+dasol729@users.noreply.github.com>
2023-12-02 18:28:59 +0900

    matching #1202

--- findsom/src/main/resources/findsom.sql
+++ findsom/src/main/resources/findsom.sql
@@ -1,35 +1,66 @@
 
+DROP SEQUENCE Sequence_commentID;
+
 CREATE SEQUENCE Sequence_commentID
-	INCREMENT BY 1
+	INCREMENT BY 2
 	START WITH 1;
+
+DROP SEQUENCE Sequence_findPostID;
 
 CREATE SEQUENCE Sequence_findPostID
 	INCREMENT BY 1
 	START WITH 1;
 
+DROP SEQUENCE Sequence_freePostID;
+
 CREATE SEQUENCE Sequence_freePostID
-	INCREMENT BY 1
-	START WITH 1;
+	INCREMENT BY 2
+	START WITH 2;
+
+DROP SEQUENCE Sequence_messageID;
 
 CREATE SEQUENCE Sequence_messageID
 	INCREMENT BY 1
 	START WITH 1;
 
+DROP SEQUENCE Sequence_notificationID;
+
 CREATE SEQUENCE Sequence_notificationID
 	INCREMENT BY 1
 	START WITH 1;
+
+DROP SEQUENCE Sequence_userID;
 
 CREATE SEQUENCE Sequence_userID
 	INCREMENT BY 1
 	START WITH 1;
 
+DROP TABLE Notification CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE MessageInfo CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Comment CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE FreeBoardPost CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE FindBoardPost CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE AuthenticationFile CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE LifePatterns CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE UserInfo CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Room CASCADE CONSTRAINTS PURGE;
+
 CREATE TABLE Comment
 (
-	commentID            VARCHAR2(18)  NOT NULL ,
+	commentID            NUMBER(4)  NOT NULL ,
 	content              VARCHAR2(100)  NULL ,
 	commentDate          DATE  NULL ,
 	userID               VARCHAR2(18)  NOT NULL ,
-	postID               VARCHAR2(18)  NULL 
+	findpostID           NUMBER(4)  NULL ,
+	freepostID           NUMBER(4)  NULL 
 );
 
 CREATE UNIQUE INDEX XPKComment ON Comment
@@ -40,45 +71,47 @@ ALTER TABLE Comment
 
 CREATE TABLE FindBoardPost
 (
-	postID               VARCHAR2(18)  NOT NULL ,
+	findpostID           NUMBER(4)  NOT NULL ,
 	isAnonymous          VARCHAR2(1)  NULL ,
 	title                VARCHAR2(20)  NULL ,
 	prefer               VARCHAR2(100)  NULL ,
 	userID               VARCHAR2(18)  NOT NULL ,
-	content              VARCHAR2(500)  NULL 
+	mycontent            VARCHAR2(500)  NULL ,
+	matecontent          VARCHAR2(500)  NULL 
 );
 
 CREATE UNIQUE INDEX XPKFindBoardPost ON FindBoardPost
-(postID   ASC);
+(findpostID   ASC);
 
 ALTER TABLE FindBoardPost
-	ADD CONSTRAINT  XPKFindBoardPost PRIMARY KEY (postID);
+	ADD CONSTRAINT  XPKFindBoardPost PRIMARY KEY (findpostID);
 
 CREATE TABLE FreeBoardPost
 (
-	postID               VARCHAR2(18)  NOT NULL ,
+	freepostID           NUMBER(4)  NOT NULL ,
 	title                VARCHAR2(50)  NULL ,
 	userID               VARCHAR2(18)  NOT NULL ,
 	isAnonymous          VARCHAR(1)  NULL ,
-	contents             VARCHAR2(500)  NULL ,
+	content              VARCHAR2(500)  NULL ,
 	category             VARCHAR2(10)  NULL 
 );
 
 CREATE UNIQUE INDEX XPKFreeBoardPost ON FreeBoardPost
-(postID   ASC);
+(freepostID   ASC);
 
 ALTER TABLE FreeBoardPost
-	ADD CONSTRAINT  XPKFreeBoardPost PRIMARY KEY (postID);
+	ADD CONSTRAINT  XPKFreeBoardPost PRIMARY KEY (freepostID);
 
 CREATE TABLE MessageInfo
 (
-	messageID            VARCHAR2(18)  NOT NULL ,
+	messageID            NUMBER(4)  NOT NULL ,
 	messageText          VARCHAR2(600)  NULL ,
 	createAt             DATE  NULL ,
 	recognizeID          VARCHAR2(18)  NULL ,
 	senderID             VARCHAR2(18)  NOT NULL ,
 	receiverID           VARCHAR2(18)  NOT NULL ,
-	postID               VARCHAR2(18)  NULL 
+	freepostID           NUMBER(4)  NULL ,
+	findpostID           NUMBER(4)  NULL 
 );
 
 CREATE UNIQUE INDEX XPKMessageInfo ON MessageInfo
@@ -87,16 +120,16 @@ CREATE UNIQUE INDEX XPKMessageInfo ON MessageInfo
 ALTER TABLE MessageInfo
 	ADD CONSTRAINT  XPKMessageInfo PRIMARY KEY (messageID);
 
-CREATE TABLE NoiseInfo
+CREATE TABLE Room
 (
 	roomInfo             VARCHAR2(18)  NOT NULL ,
 	count                VARCHAR2(18)  NULL 
 );
 
-CREATE UNIQUE INDEX XPKNoiseInfo ON NoiseInfo
+CREATE UNIQUE INDEX XPKNoiseInfo ON Room
 (roomInfo   ASC);
 
-ALTER TABLE NoiseInfo
+ALTER TABLE Room
 	ADD CONSTRAINT  XPKNoiseInfo PRIMARY KEY (roomInfo);
 
 CREATE TABLE UserInfo
@@ -108,7 +141,8 @@ CREATE TABLE UserInfo
 	nickname             VARCHAR2(10)  NULL ,
 	authentication       VARCHAR2(1)  DEFAULT 0  NULL ,
 	roomInfo             VARCHAR2(18)  NULL ,
-	isRecruite           VARCHAR2(10)  NULL 
+	isRecruite           VARCHAR2(10)  NULL ,
+	phone                CHAR(18)  NULL 
 );
 
 CREATE UNIQUE INDEX XPKUserInfo ON UserInfo
@@ -116,36 +150,6 @@ CREATE UNIQUE INDEX XPKUserInfo ON UserInfo
 
 ALTER TABLE UserInfo
 	ADD CONSTRAINT  XPKUserInfo PRIMARY KEY (userID);
-
-CREATE TABLE Notification
-(
-	notificationID       CHAR(18)  NOT NULL ,
-	userID               VARCHAR2(18)  NOT NULL ,
-	notiType             VARCHAR2(18)  NULL ,
-	notiTypeID           VARCHAR2(18)  NULL ,
-	isChecked            VARCHAR2(2)  NULL ,
-	commentID            VARCHAR2(18)  NULL ,
-	messageID            VARCHAR2(18)  NULL ,
-	postID               VARCHAR2(18)  NULL 
-);
-
-CREATE UNIQUE INDEX XPKNotificationBox ON Notification
-(notificationID   ASC);
-
-ALTER TABLE Notification
-	ADD CONSTRAINT  XPKNotificationBox PRIMARY KEY (notificationID);
-
-CREATE TABLE LifePatterns
-(
-	lifePattern          VARCHAR2(20)  NULL ,
-	userID               VARCHAR2(18)  NOT NULL 
-);
-
-CREATE UNIQUE INDEX XPKLifePatterns ON LifePatterns
-(userID   ASC);
-
-ALTER TABLE LifePatterns
-	ADD CONSTRAINT  XPKLifePatterns PRIMARY KEY (userID);
 
 CREATE TABLE AuthenticationFile
 (
@@ -159,17 +163,48 @@ CREATE UNIQUE INDEX XPKAuthenticationFile ON AuthenticationFile
 ALTER TABLE AuthenticationFile
 	ADD CONSTRAINT  XPKAuthenticationFile PRIMARY KEY (userID);
 
+CREATE TABLE LifePatterns
+(
+	lifePattern          VARCHAR2(20)  NULL ,
+	userID               VARCHAR2(18)  NOT NULL 
+);
+
+CREATE UNIQUE INDEX XPKLifePatterns ON LifePatterns
+(userID   ASC);
+
+ALTER TABLE LifePatterns
+	ADD CONSTRAINT  XPKLifePatterns PRIMARY KEY (userID);
+
+CREATE TABLE Notification
+(
+	notificationID       NUMBER(4)  NOT NULL ,
+	notiType             VARCHAR2(18)  NULL ,
+	notiTypeID           VARCHAR2(18)  NULL ,
+	isChecked            VARCHAR2(2)  NULL ,
+	commentID            NUMBER(4)  NULL ,
+	messageID            NUMBER(4)  NULL ,
+	postID               NUMBER(4)  NULL ,
+	receiverID           VARCHAR2(18)  NOT NULL ,
+	senderID             VARCHAR2(18)  NOT NULL 
+);
+
+CREATE UNIQUE INDEX XPKNotificationBox ON Notification
+(notificationID   ASC);
+
+ALTER TABLE Notification
+	ADD CONSTRAINT  XPKNotificationBox PRIMARY KEY (notificationID);
+
 ALTER TABLE Comment
 	ADD (
 CONSTRAINT R_11 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
 
 ALTER TABLE Comment
 	ADD (
-CONSTRAINT R_34 FOREIGN KEY (postID) REFERENCES FindBoardPost (postID) ON DELETE SET NULL);
+CONSTRAINT R_34 FOREIGN KEY (findpostID) REFERENCES FindBoardPost (findpostID) ON DELETE SET NULL);
 
 ALTER TABLE Comment
 	ADD (
-CONSTRAINT R_35 FOREIGN KEY (postID) REFERENCES FreeBoardPost (postID) ON DELETE SET NULL);
+CONSTRAINT R_35 FOREIGN KEY (freepostID) REFERENCES FreeBoardPost (freepostID) ON DELETE SET NULL);
 
 ALTER TABLE FindBoardPost
 	ADD (
@@ -189,15 +224,23 @@ CONSTRAINT R_28 FOREIGN KEY (receiverID) REFERENCES UserInfo (userID));
 
 ALTER TABLE MessageInfo
 	ADD (
-CONSTRAINT R_32 FOREIGN KEY (postID) REFERENCES FreeBoardPost (postID) ON DELETE SET NULL);
+CONSTRAINT R_32 FOREIGN KEY (freepostID) REFERENCES FreeBoardPost (freepostID) ON DELETE SET NULL);
 
 ALTER TABLE MessageInfo
 	ADD (
-CONSTRAINT R_33 FOREIGN KEY (postID) REFERENCES FindBoardPost (postID) ON DELETE SET NULL);
+CONSTRAINT R_33 FOREIGN KEY (findpostID) REFERENCES FindBoardPost (findpostID) ON DELETE SET NULL);
 
 ALTER TABLE UserInfo
 	ADD (
-CONSTRAINT R_20 FOREIGN KEY (roomInfo) REFERENCES NoiseInfo (roomInfo) ON DELETE SET NULL);
+CONSTRAINT R_20 FOREIGN KEY (roomInfo) REFERENCES Room (roomInfo) ON DELETE SET NULL);
+
+ALTER TABLE AuthenticationFile
+	ADD (
+CONSTRAINT R_7 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
+
+ALTER TABLE LifePatterns
+	ADD (
+CONSTRAINT R_6 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
 
 ALTER TABLE Notification
 	ADD (
@@ -209,16 +252,12 @@ CONSTRAINT R_25 FOREIGN KEY (messageID) REFERENCES MessageInfo (messageID) ON DE
 
 ALTER TABLE Notification
 	ADD (
-CONSTRAINT R_26 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
+CONSTRAINT R_29 FOREIGN KEY (postID) REFERENCES FindBoardPost (findpostID) ON DELETE SET NULL);
 
 ALTER TABLE Notification
 	ADD (
-CONSTRAINT R_29 FOREIGN KEY (postID) REFERENCES FindBoardPost (postID) ON DELETE SET NULL);
+CONSTRAINT R_41 FOREIGN KEY (receiverID) REFERENCES UserInfo (userID));
 
-ALTER TABLE LifePatterns
+ALTER TABLE Notification
 	ADD (
-CONSTRAINT R_6 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
-
-ALTER TABLE AuthenticationFile
-	ADD (
-CONSTRAINT R_7 FOREIGN KEY (userID) REFERENCES UserInfo (userID));
+CONSTRAINT R_43 FOREIGN KEY (senderID) REFERENCES UserInfo (userID));
